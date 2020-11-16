a85ef20c69fbf4c3fd46cee77a148d103a6a4456
Helen-Noe <helenmyatnoe@gmail.com>
2020-11-16 15:45:11 +1100

    Profile component gettinf detail of cat org

---
+++ src/Profile.js
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import "./font-awesome/css/font-awesome.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe, faComments } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import bg from "./image/banner.png";
+import Repos from "./Repos";
+
+class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getUser();
+  }
+
+  getUser() {
+    fetch(`https://api.github.com/orgs/catalyst`)
+      .then((response) => response.json())
+      .then(
+        (result) => {
+          // console.log(result);
+          this.setState({
+            isLoaded: true,
+            data: result,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
+  }
+
+  render() {
+    const { error, isLoaded, data } = this.state;
+    if (error) {
+      return <div> Error: {error.message} </div>;
+    } else if (!isLoaded) {
+      return <div> Loading.. </div>;
+    } else {
+      return (
+        <div id="banner">
+          <ul>
+            <li>
+              <h1>{data.name}</h1>
+            </li>
+            <li>
+              <p>{data.description}</p>
+            </li>
+            <li>
+              <a href="https://www.catalyst-au.net/" className="globe">
+                <FontAwesomeIcon
+                  style={{ color: "#ffffff" }}
+                  icon={faGlobe}
+                  size="2x"
+                />
+              </a>
+              <a href={data.blog} className="comments">
+                <FontAwesomeIcon
+                  style={{ color: "#ffffff" }}
+                  icon={faComments}
+                  size="2x"
+                />
+              </a>
+              <a href={data.html_url} className="github">
+                <FontAwesomeIcon
+                  style={{ color: "#ffffff" }}
+                  icon={faGithub}
+                  size="2x"
+                />
+              </a>
+            </li>
+            <li id="repoNumber">Repos: {data.public_repos}</li>
+            <li> Location: {data.location}</li>
+            <li className="blank"></li>
+          </ul>
+        </div>
+      );
+    }
+  }
+}
+
+export default Profile;
